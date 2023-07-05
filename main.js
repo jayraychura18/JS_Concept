48ab7f15c3ed212518f041fb3b7c2da527314683
Jay <jayraychura18@gmail.com>
2023-07-05 22:07:55 +0530

    Hey This my Project

---
+++ main.js
@@ -0,0 +1,129 @@
+// let name='sindhu';
+
+// function getName(){
+//     let name='anil';
+//     console.log(name);
+// }
+// getName();
+
+
+// let data=[2,9,0,10]
+// // data.shift(1)
+// // data.unshift(1)
+// // data.push(1)
+// // data.pop(1)
+// console.warn(data);
+
+// let a=[1,2,3,4];
+// let b=[5,6,7];
+// let c=[...a,...b];
+// console.warn(c)
+
+// let a=1;
+// let c=2;
+// console.warn(--c ==+a)
+
+
+// console.warn([[[[[]]]]])
+
+
+// function powerMod(a, b, c) {
+//     var result = Math.pow(a, b) % c;
+//     return result;
+// }
+
+// var a = parseInt(prompt("Enter the value of a: "));
+// var b = parseInt(prompt("Enter the value of b: "));
+// var c = parseInt(prompt("Enter the value of c: "));
+
+// var result = powerMod(a, b, c);
+// console.log("Result: " + result);
+
+
+// function powerMod(a,b,c){
+//     var result=Math.pow(a,b)%c
+//     return result        
+// }
+
+// var a=parseInt(prompt("Enter the value of a:"))
+// console.log("a:"+a)
+// var b=parseInt(prompt("Enter the value of b:"))
+// console.log("b:"+b)
+// var c=parseInt(prompt("Enter the value of c:"))
+// console.log("c:"+c)
+// var result=powerMod(a,b,c)
+// console.log("Result:"+result)
+
+
+// let data="size";
+
+// const bird={
+//     size:"small",
+// };
+
+// console.log(bird[data])
+// console.log(bird["size"]);
+// console.log(bird.size);
+// console.log(bird.data);
+
+// var x;
+// var x=10;
+// var x=90;
+// var x=67;
+// console.log(x);
+
+// var x;
+// let x=10;
+// console.log(x)
+
+
+// let x=5;
+// var x=10;
+// console.log(x)
+
+// let a=3;
+// let b=new Number(3);
+
+// console.log(a==b);
+// console.log(a===b);  
+//here in above line type checking of a and b and ther's type of b is object and type of a is number so it give the result as a true.
+
+// console.log(!!null);
+// console.log(!!"");
+// console.log(!!1);
+
+// console.log([..."Anil"]);
+
+// let person={name:'peter'};
+
+// const members=[person];
+// person=null;
+// console.log(members);
+
+// let data=[1,2,3].map(num=>{
+//     if(typeof num==='number') return;
+//     return num*2;
+// })
+// console.log(data);
+
+// function getInfo(member){
+//     member.name='Anil';
+// }
+
+// const person={name:'Sarah'};
+
+// getInfo(person);
+
+// console.log(person);
+
+// let n=prompt("enter the number which you want factorial of that number")
+// n=Number.parseInt(n)
+
+// function factorial(n){
+//     if(n===0||n===1){
+//         return 1;        
+//     }else{
+//      return n*factorial(n-1)
+//     }
+// }
+// console.warn(`the factorial of ${n} is ${factorial(n)}`)
